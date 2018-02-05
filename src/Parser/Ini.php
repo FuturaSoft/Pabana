0a5054575d641cc25bc68eace3f7a56773bdf4a8
t.forax <t.forax@futurasoft.fr>
2018-02-05 15:05:44 +0100

    Add a Parser to convert Ini to Array

--- src/Parser/Ini.php
+++ src/Parser/Ini.php
@@ -1,24 +1,35 @@
 <?php
+/**
+ * Pabana : PHP Framework (https://pabana.futurasoft.fr)
+ * Copyright (c) FuturaSoft (https://futurasoft.fr)
+ *
+ * Licensed under BSD-3-Clause License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) FuturaSoft (https://futurasoft.fr)
+ * @link          https://pabana.futurasoft.fr Pabana Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
+ */
 namespace Pabana\Parser;
 
-use Pabana\Error\Error;
-
-Class Ini
+class Ini
 {
-	private $sFilename;
+    private $sFilename;
 
-	public function load($sFilename)
-	{
-		if(!file_exists($sFilename)) {
-			throw new Error('Ini file "' . $sFilename . '" doesn\'t exist.');
-			return false;
-		}
-		$this->sFilename = $sFilename;
-		return $this;
-	}
+    public function load($sFilename)
+    {
+        if (!file_exists($sFilename)) {
+            throw new \Exception('Ini file "' . $sFilename . '" doesn\'t exist.');
+            return false;
+        }
+        $this->sFilename = $sFilename;
+        return $this;
+    }
 
-	public function toArray($bProcessSection = false)
-	{
-		return parse_ini_file($this->sFilename, $bProcessSection, INI_SCANNER_TYPED);
-	}
+    public function toArray($bProcessSection = false)
+    {
+        return parse_ini_file($this->sFilename, $bProcessSection, INI_SCANNER_TYPED);
+    }
 }
