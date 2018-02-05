969f72bb241f298fb6c02d1bc010cbb252d59613
t.forax <t.forax@futurasoft.fr>
2018-02-05 14:59:26 +0100

    Change default database connexion storage.

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -1,58 +1,71 @@
 <?php
+/**
+ * Pabana : PHP Framework (https://pabana.futurasoft.fr)
+ * Copyright (c) FuturaSoft (https://futurasoft.fr)
+ *
+ * Licensed under BSD-3-Clause License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) FuturaSoft (https://futurasoft.fr)
+ * @link          https://pabana.futurasoft.fr Pabana Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
+ */
 namespace Pabana\Database;
 
-use Pabana\Database\ConnectionCollection;
 use Pabana\Database\Statement;
-use Pabana\Debug\Error;
 
 class Connection
 {
-    public $Datasource = null;
-	private $oPdo = null;
+    public $Datasource;
+    private $oPdo;
+    private $sName;
 
-    public function __construct($oDatasource, $bAutoConnect = true, $bAutoRegister = true)
+    public function __construct($oDatasource, $sName = '', $bAutoConnect = true)
     {
         $this->Datasource = $oDatasource;
-        if ($bAutoConnect) {
-            $this->connect($bAutoRegister);
+        if (empty($sName) === true) {
+            $sName = $this->Datasource->getName();
+        }
+        $this->setName($sName);
+        if ($bAutoConnect === true) {
+            $this->connect();
         }
     }
 
-	public function connect($bAutoRegister = true)
+    public function connect()
     {
-		try {
-			$this->oPdo = new \PDO($this->Datasource->getDsn(),
+        try {
+            $this->oPdo = new \PDO(
+                $this->Datasource->getDsn(),
                                    $this->Datasource->getUser(),
                                    $this->Datasource->getPassword(),
-                                   $this->Datasource->getOption());
-            if ($bAutoRegister) {
-                ConnectionCollection::add($this->Datasource->getName(), $this);
-            }
-		}
-        catch (PDOException $e) {
-        	throw new Error($e->getMessage());
+                                   $this->Datasource->getOption()
+            );
+        } catch (PDOException $e) {
+            throw new \Exception($e->getMessage());
             return false;
         }
-	}
+    }
 
-	public function disconnect()
+    public function disconnect()
     {
-		if($this->isConnected()) {
-			$this->oPdo = null;
-			return true;
-		} else {
+        if ($this->isConnected()) {
+            $this->oPdo = null;
+            return true;
+        } else {
             return false;
         }
-	}
+    }
 
     public function exec($sQuery)
     {
-        if($this->isConnected()) {
+        if ($this->isConnected()) {
             try {
                 return $this->oPdo->exec($sQuery);
-            }
-            catch (PDOException $e) {
-                throw new Error($e->getMessage());
+            } catch (PDOException $e) {
+                throw new \Exception($e->getMessage());
                 return false;
             }
         } else {
@@ -60,34 +73,56 @@ class Connection
         }
     }
 
-	public function getPdoObject()
+    public function getPdoObject()
     {
-		return $this->oPdo;
-	}
+        if ($this->isConnected()) {
+            return $this->oPdo;
+        } else {
+            return false;
+        }
+    }
 
-	public function isConnected()
+    public function getName()
     {
-		return !empty($this->oPdo);
-	}
+        return $this->sName;
+    }
+
+    public function isConnected()
+    {
+        return isset($this->oPdo);
+    }
 
     public function lastInsertId()
     {
-        return $this->oPdo->lastInsertId();
-    }
-
-	public function query($sQuery)
-    {
-        if($this->isConnected()) {
+        if ($this->isConnected()) {
             try {
-                $oStatement = $this->oPdo->query($sQuery);
-                return new Statement($oStatement);
-            }
-            catch (PDOException $e) {
-            	throw new Error($e->getMessage());
+                return $this->oPdo->lastInsertId();
+            } catch (PDOException $e) {
+                throw new \Exception($e->getMessage());
                 return false;
             }
         } else {
             return false;
         }
+    }
+
+    public function query($sQuery)
+    {
+        if ($this->isConnected()) {
+            try {
+                $oStatement = $this->oPdo->query($sQuery);
+                return new Statement($oStatement);
+            } catch (PDOException $e) {
+                throw new \Exception($e->getMessage());
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function setName($sName)
+    {
+        $this->sName = $sName;
     }
 }
--- src/Database/ConnectionCollection.php
+++ src/Database/ConnectionCollection.php
@@ -1,33 +1,77 @@
 <?php
+/**
+ * Pabana : PHP Framework (https://pabana.futurasoft.fr)
+ * Copyright (c) FuturaSoft (https://futurasoft.fr)
+ *
+ * Licensed under BSD-3-Clause License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) FuturaSoft (https://futurasoft.fr)
+ * @link          https://pabana.futurasoft.fr Pabana Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
+ */
 namespace Pabana\Database;
 
-use Pabana\Database\Database;
-
-Class ConnectionCollection
+class ConnectionCollection
 {
-	private static $armConnectionList = array();
-	private static $sDefaultConnectionName = null;
+    private static $aroConnectionList;
+    private static $sDefaultConnectionName;
 
-	public static function add($sConnectionName, $oConnection)
-	{
-		self::$armConnectionList[$sConnectionName] = $oConnection;
-		if ($oConnection->Datasource->isDefault()) {
-			self::$sDefaultConnectionName = $sConnectionName;
-		}
-	}
+    public static function add($cnxConnection, $bSetAsDefault = false)
+    {
+        $sConnectionName = $cnxConnection->getName();
+        self::$aroConnectionList[$sConnectionName] = $cnxConnection;
+        if ($bSetAsDefault === true) {
+            self::setDefault($sConnectionName);
+        }
+    }
 
-	public static function get($sConnectionName)
-	{
-		return self::$armConnectionList[$sConnectionName];
-	}
+    public static function exists($sConnectionName)
+    {
+        return isset(self::$aroConnectionList[$sConnectionName]);
+    }
 
-	public static function getDefault()
-	{
-		return self::$armConnectionList[self::$sDefaultConnectionName];
-	}
+    public static function existsDefault()
+    {
+        return isset(self::$sDefaultConnectionName);
+    }
 
-	public static function getList()
-	{
-		return self::$armConnectionList;
-	}
+    public static function get($sConnectionName)
+    {
+        if (self::exists($sConnectionName) === true) {
+            return self::$aroConnectionList[$sConnectionName];
+        } else {
+            throw new Exception('Datasource "' . $sConnectionName . '" isn\'t defined in DatasourceCollection');
+            return false;
+        }
+    }
+
+    public static function getDefault()
+    {
+        if (self::existsDefault()) {
+            $sDefaultConnectionName = self::$sDefaultConnectionName;
+            return self::$aroConnectionList[$sDefaultConnectionName];
+        } else {
+            throw new Exception('Datasource "' . $sConnectionName . '" isn\'t defined in DatasourceCollection');
+            return false;
+        }
+    }
+
+    public static function getAll()
+    {
+        return self::$aroConnectionList;
+    }
+
+    public static function setDefault($sConnectionName, $bForce = true)
+    {
+        if ($bForce === false && self::existsDefault() === true) {
+            throw new Exception('A default Datasource is already defined.');
+            return false;
+        } else {
+            self::$sDefaultConnectionName = $sConnectionName;
+            return true;
+        }
+    }
 }
