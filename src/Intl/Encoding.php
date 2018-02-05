77a4f8ef04a9b1d54bdeec1df263a9e375ef2042
t.forax <t.forax@futurasoft.fr>
2018-02-05 15:03:02 +0100

    Add charset conversion class

--- src/Intl/Encoding.php
+++ src/Intl/Encoding.php
@@ -1,38 +1,50 @@
 <?php
+/**
+ * Pabana : PHP Framework (https://pabana.futurasoft.fr)
+ * Copyright (c) FuturaSoft (https://futurasoft.fr)
+ *
+ * Licensed under BSD-3-Clause License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) FuturaSoft (https://futurasoft.fr)
+ * @link          https://pabana.futurasoft.fr Pabana Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
+ */
 namespace Pabana\Intl;
 
 class Encoding
 {
-	public function detect($sValue)
-	{
-		return mb_detect_encoding($sValue);
-	}
+    public function detect($sValue)
+    {
+        return mb_detect_encoding($sValue);
+    }
 
-	public function convert($mValue, $sInCharset = 'auto', $sOutCharset = 'auto', $bTranslit = true, $bIgnore = true)
-	{
-		$mReturn = $mValue;
-		if (is_string($mValue)) {
-			if ($sInCharset == 'auto') {
-				$sInCharset = $this->detect($mValue);
-			}
-			if ($sOutCharset == 'auto') {
-				$sOutCharset = Configuration::read('application.encoding');
-			}
-			if ($bTranslit === true) {
-				$sOutCharset .= '//TRANSLIT';
-			} else if ($bIgnore === true) {
-				$sOutCharset .= '//IGNORE';
-			}
-			$mReturn = @iconv($sInCharset, $sOutCharset, $mValue);
-		} else if (is_array($mValue)) {
-			$mReturn = array();
-			foreach ($mValue as $mArrayKey => $mArrayValue) {
-				$mArrayKey = $this->convert($mArrayKey, $sInCharset, $sOutCharset, $bTranslit, $bIgnore);
-				$mArrayValue = $this->convert($mArrayValue, $sInCharset, $sOutCharset, $bTranslit, $bIgnore);
-				$mReturn[$mArrayKey] = $mArrayValue;
-			}
-		}
-		return $mReturn;
-	}
+    public function convert($mValue, $sInCharset = 'auto', $sOutCharset = 'auto', $bTranslit = true, $bIgnore = true)
+    {
+        $mReturn = $mValue;
+        if (is_string($mValue)) {
+            if ($sInCharset == 'auto') {
+                $sInCharset = $this->detect($mValue);
+            }
+            if ($sOutCharset == 'auto') {
+                $sOutCharset = Configuration::read('application.encoding');
+            }
+            if ($bTranslit === true) {
+                $sOutCharset .= '//TRANSLIT';
+            } elseif ($bIgnore === true) {
+                $sOutCharset .= '//IGNORE';
+            }
+            $mReturn = @iconv($sInCharset, $sOutCharset, $mValue);
+        } elseif (is_array($mValue)) {
+            $mReturn = array();
+            foreach ($mValue as $mArrayKey => $mArrayValue) {
+                $mArrayKey = $this->convert($mArrayKey, $sInCharset, $sOutCharset, $bTranslit, $bIgnore);
+                $mArrayValue = $this->convert($mArrayValue, $sInCharset, $sOutCharset, $bTranslit, $bIgnore);
+                $mReturn[$mArrayKey] = $mArrayValue;
+            }
+        }
+        return $mReturn;
+    }
 }
-?>
