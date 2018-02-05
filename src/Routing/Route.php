c7dc1926f0dd53ac3d48f7faeebcfa45a7e53ad4
t.forax <t.forax@futurasoft.fr>
2018-02-05 15:06:23 +0100

    Add a routing module to manager routage between default route and custom route

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -1,38 +1,57 @@
 <?php
+/**
+ * Pabana : PHP Framework (https://pabana.futurasoft.fr)
+ * Copyright (c) FuturaSoft (https://futurasoft.fr)
+ *
+ * Licensed under BSD-3-Clause License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) FuturaSoft (https://futurasoft.fr)
+ * @link          https://pabana.futurasoft.fr Pabana Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
+ */
 namespace Pabana\Routing;
 
-Class Route {
-	private $_sRoute = null;
-	private $_sController = 'index';
-	private $_sAction = 'index';
-	private $_arsParamList = null;
+class Route
+{
+    private $_sRoute = null;
+    private $_sController = 'index';
+    private $_sAction = 'index';
+    private $_arsParamList = null;
 
-	public function __construct($sRoute, $arsOption = array()) {
-		$this->_sRoute = $sRoute;
-		if(isset($arsOption['controller'])) {
-			$this->_sController = $arsOption['controller'];
-		}
-		if(isset($arsOption['action'])) {
-			$this->_sAction = $arsOption['action'];
-		}
-		if(isset($arsOption['param'])) {
-			$this->_arsParamList = $arsOption['param'];
-		}
-	}
+    public function __construct($sRoute, $arsOption = array())
+    {
+        $this->_sRoute = $sRoute;
+        if (isset($arsOption['controller'])) {
+            $this->_sController = $arsOption['controller'];
+        }
+        if (isset($arsOption['action'])) {
+            $this->_sAction = $arsOption['action'];
+        }
+        if (isset($arsOption['param'])) {
+            $this->_arsParamList = $arsOption['param'];
+        }
+    }
 
-	public function getRoute() {
-		return $this->_sRoute;
-	}
+    public function getRoute()
+    {
+        return $this->_sRoute;
+    }
 
-	public function getController() {
-		return $this->_sController;
-	}
+    public function getController()
+    {
+        return $this->_sController;
+    }
 
-	public function getAction() {
-		return $this->_sAction;
-	}
+    public function getAction()
+    {
+        return $this->_sAction;
+    }
 
-	public function getParamList() {
-		return $this->_arsParamList;
-	}
+    public function getParamList()
+    {
+        return $this->_arsParamList;
+    }
 }
--- src/Routing/RouteCollection.php
+++ src/Routing/RouteCollection.php
@@ -1,56 +1,87 @@
 <?php
+/**
+ * Pabana : PHP Framework (https://pabana.futurasoft.fr)
+ * Copyright (c) FuturaSoft (https://futurasoft.fr)
+ *
+ * Licensed under BSD-3-Clause License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) FuturaSoft (https://futurasoft.fr)
+ * @link          https://pabana.futurasoft.fr Pabana Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause License
+ */
 namespace Pabana\Routing;
 
 use Pabana\Routing\Route;
 
-Class RouteCollection {
-	private static $arsRouteCollection;
-	private static $sFallbackAction = 'index';
-	private static $sFallbackController = 'Error';
-	private static $arcSeparator = array('/', '?', '&', '=');
+class RouteCollection
+{
+    private static $arsRouteCollection;
+    private static $sFallbackAction = 'index';
+    private static $sFallbackController = 'Error';
+    private static $arcSeparator = array('/', '?', '&', '=');
+    private static $cDefaultSeparator = '/';
 
-	public static function add($sRoute, $arsOption) {
-		self::$arsRouteCollection[] = new Route($sRoute, $arsOption);
-	}
+    public static function add($sRoute, $arsOption)
+    {
+        self::$arsRouteCollection[] = new Route($sRoute, $arsOption);
+    }
 
-	public static function getFallbackAction()
-	{
-		return self::$sFallbackAction;
-	}
+    public static function getFallbackAction()
+    {
+        return self::$sFallbackAction;
+    }
 
-	public static function getFallbackController()
-	{
-		return self::$sFallbackController;
-	}
+    public static function getFallbackController()
+    {
+        return self::$sFallbackController;
+    }
 
-	public static function getAll() {
-		return self::$arsRouteCollection;
-	}
+    public static function getAll()
+    {
+        return self::$arsRouteCollection;
+    }
 
-	public static function getSeparator()
-	{
-		return self::$arcSeparator;
-	}
+    public static function getDefaultSeparator()
+    {
+        return self::$cDefaultSeparator;
+    }
 
-	public static function setFallback($sController, $sAction) {
-		self::$sFallbackAction = $sAction;
-		self::$sFallbackController = $sController;
-	}
+    public static function getSeparator()
+    {
+        return self::$arcSeparator;
+    }
 
-	public static function setSeparator($mSeparator, $bMerge = true) {
-		if (is_array($mSeparator)) {
-			if ($bMerge === true) {
-				$arcSeparator = $mSeparator + $arcSeparator;
-			} else {
-				$arcSeparator = $mSeparator;
-			}
-		} else {
-			if ($bMerge === true) {
-				$arcSeparator[] = $mSeparator;
-			} else {
-				$arcSeparator = array($mSeparator);
-			}
-		}
-		self::$arcSeparator = $arcSeparator;
-	}
+    public static function setFallback($sController, $sAction)
+    {
+        self::$sFallbackAction = $sAction;
+        self::$sFallbackController = $sController;
+    }
+
+    public static function setDefaultSeparator($cSeparator, $bMerge = true)
+    {
+        self::$cDefaultSeparator = $cSeparator;
+        self::setSeparator($cSeparator, $bMerge);
+    }
+
+    public static function setSeparator($mSeparator, $bMerge = true)
+    {
+        if (is_array($mSeparator)) {
+            if ($bMerge === true) {
+                self::$arcSeparator = $mSeparator + self::$arcSeparator;
+            } else {
+                self::$arcSeparator = $mSeparator;
+            }
+        } else {
+            if ($bMerge === true) {
+                if (in_array($mSeparator, self::$arcSeparator) === false) {
+                    self::$arcSeparator[] = $mSeparator;
+                }
+            } else {
+                self::$arcSeparator = array($mSeparator);
+            }
+        }
+    }
 }
