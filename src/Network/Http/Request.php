70009a60052d33703cc8ed6d2f8d14778659f932
t.forax <t.forax@futurasoft.fr>
2018-03-13 19:45:31 +0100

    Add header list Add Autorization support Transform old method to get method or is method

--- src/Network/Http/Request.php
+++ src/Network/Http/Request.php
@@ -22,24 +22,395 @@ namespace Pabana\Network\Http;
 class Request
 {
     /**
-     * Check accept field
+     * @var    Array List of Request headers
+     * @since   1.1
+     */
+    private $headerList = array();
+
+    /**
+     * Constructor
+     *
+     * @since   1.1
+     */
+    public function __construct()
+    {
+        $this->headerList = $this->getHeaderList();
+    }
+
+    /**
+     * Check accept charset
      *
      * @since   1.0
-     * @param   string $sContent Accept header.
-     * @param   string $sType Accept test.
+     * @deprecated deprecated since version 1.1
+     * @param   string $charset Test charset.
      * @return  bool True if test is ok else false.
      */
-    private function accept($sContent, $sType)
+    public function acceptCharset($charset)
     {
-        $arsAcceptMimetype = $this->parseAccept($sContent);
-        if (empty($sType)) {
-            return $arsAcceptMimetype;
+        return $this->isAcceptCharset($charset);
+    }
+
+    /**
+     * Check accept encoding
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @param   string $encoding Test encoding.
+     * @return  bool True if test is ok else false.
+     */
+    public function acceptEncoding($encoding)
+    {
+        return $this->isAcceptEncoding($encoding);
+    }
+
+    /**
+     * Check accept language
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @param   string $language Test language.
+     * @return  bool True if test is ok else false.
+     */
+    public function acceptLanguage($language)
+    {
+        return $this->isAcceptLanguage($language);
+    }
+
+    /**
+     * Check accept mimetype
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @param   string $mimetype Test mimetype.
+     * @return  bool True if test is ok else false.
+     */
+    public function acceptMimetype($mimetype)
+    {
+        return $this->isAcceptMimetype($mimetype);
+    }
+
+    /**
+     * Get client IP
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @return  string Return client IP.
+     */
+    public function clientIp()
+    {
+        return $this->getClientIp();
+    }
+
+    /**
+     * Get domain
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @param   string $tldLength Length of Top Level Domain. (by default 1)
+     * @return  string Return domain.
+     */
+    public function domain($tldLength = 1)
+    {
+        return $this->getDomain($tldLength);
+    }
+
+    /**
+     * Get Authorization method
+     *
+     * @since   1.1
+     * @return  string Return Authorization method.
+     */
+    public function getAuthorizationMethod()
+    {
+        if ($this->hasHeader('Authorization')) {
+            $authorization = $this->getHeader('Authorization');
+            return strstr($authorization, ' ', true);
+        }
+        return false;
+    }
+
+    /**
+     * Get Authorization value
+     *
+     * @since   1.1
+     * @return  string Return Authorization value.
+     */
+    public function getAuthorizationValue()
+    {
+        if ($this->hasHeader('Authorization')) {
+            $authorization = $this->getHeader('Authorization');
+            return strstr($authorization, ' ');
+        }
+        return false;
+    }
+
+    /**
+     * Get client IP
+     *
+     * @since   1.1
+     * @return  string Return client IP.
+     */
+    public function getClientIp()
+    {
+        if (!empty($this->getHeader('X-Forwarded-For'))) {
+            $ip = preg_replace('/(?:,.*)/', '', $this->hasHeader('X-Forwarded-For'));
+        } elseif (!empty($this->getHeader('Client-Ip'))) {
+            $ip = $this->getHeader('Client-Ip');
         } else {
-            foreach ($arsAcceptMimetype as $arsAcceptList) {
-                if (in_array($sType, $arsAcceptList)) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return trim($ip);
+    }
+
+    /**
+     * Get domain
+     *
+     * @since   1.1
+     * @param   string $tldLength Length of Top Level Domain. (by default 1)
+     * @return  string Return domain.
+     */
+    public function getDomain($tldLength = 1)
+    {
+        $segmentList = explode('.', $this->getHost());
+        $domain = array_slice($segmentList, -1 * ($tldLength + 1));
+        return implode('.', $domain);
+    }
+
+    /**
+     * Get header value
+     *
+     * @since   1.1
+     * @param   string $headerName Name of header
+     * @return  mixed Return value of header if exist or false if not exist.
+     */
+    public function getHeader($headerName)
+    {
+        if ($this->hasHeader($headerName)) {
+            return $this->headerList[$headerName];
+        }
+        return false;
+    }
+
+    /**
+     * Get header list
+     *
+     * @since   1.1
+     * @return  array Return array of header for current request.
+     */
+    public function getHeaderList()
+    {
+        if (!function_exists('getallheaders')) {
+            $headers = [];
+            $copy_server = array(
+                'CONTENT_TYPE'   => 'Content-Type',
+                'CONTENT_LENGTH' => 'Content-Length',
+                'CONTENT_MD5'    => 'Content-Md5'
+            );
+            foreach ($_SERVER as $key => $value) {
+                if (substr($key, 0, 5) === 'HTTP_') {
+                    $key = substr($key, 5);
+                    if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
+                        $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+                        $headers[$key] = $value;
+                    }
+                } elseif (isset($copy_server[$key])) {
+                    $headers[$copy_server[$key]] = $value;
+                }
+            }
+            if (!isset($headers['Authorization'])) {
+                if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                    $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+                } elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+                    $basic_pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
+                    $headers['Authorization'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $basic_pass);
+                } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+                    $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+                }
+            }
+            return $headers;
+        } else {
+            return getallheaders();
+        }
+    }
+
+    /**
+     * Get host
+     *
+     * @since   1.1
+     * @return  string Return host.
+     */
+    public function getHost()
+    {
+        return $this->getHeader('host');
+    }
+
+    /**
+     * Get request method
+     *
+     * @since   1.1
+     * @return  string Return request method (GET, POST, ...).
+     */
+    public function getMethod()
+    {
+        if ($this->hasHeader('X-Http-Method-Override')) {
+            return $this->getHeader('X-Http-Method-Override');
+        }
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    /**
+     * Get request port
+     *
+     * @since   1.1
+     * @return  integer Return request port.
+     */
+    public function getPort()
+    {
+        return $_SERVER['SERVER_PORT'];
+    }
+
+    /**
+     * Get referer
+     *
+     * @since   1.1
+     * @return  string Return referer.
+     */
+    public function getReferer()
+    {
+        return $this->getHeader('Referer');
+    }
+
+    /**
+     * Get request scheme
+     *
+     * @since   1.1
+     * @return  string Return request scheme (http or https).
+     */
+    public function getScheme()
+    {
+        if (isset($_SERVER['HTTPS']) || is_empty($_SERVER['HTTPS'])) {
+            return 'https';
+        } else {
+            return 'http';
+        }
+    }
+
+    /**
+     * Get subdomain
+     *
+     * @since   1.1
+     * @param   string $tldLength Length of Top Level Domain. (by default 1)
+     * @return  string Return subdomain.
+     */
+    public function getSubdomain($tldLength = 1)
+    {
+        $segmentList = explode('.', $this->getHost());
+        return array_slice($segmentList, 0, -1 * ($tldLength + 1));
+    }
+
+    /**
+     * Get url
+     *
+     * @since   1.1
+     * @return  string Return request url.
+     */
+    public function getUrl()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * Get user agent
+     *
+     * @since   1.1
+     * @return  string Return user agent.
+     */
+    public function getUserAgent()
+    {
+        return $this->getHeader('User-Agent');
+    }
+
+    /**
+     * Check if header exist
+     *
+     * @since   1.1
+     * @param   string $headerName Name of header
+     * @return  mixed Return true if exist or false if not exist.
+     */
+    public function hasHeader($headerName)
+    {
+        return isset($this->headerList[$headerName]);
+    }
+
+    /**
+     * Get host
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @return  string Return host.
+     */
+    public function host()
+    {
+        return $this->getHost();
+    }
+
+    /**
+     * Check method, ajax, json and xml
+     *
+     * @since   1.0
+     * @deprecated deprecated since version 1.1
+     * @param   string $sTest Type of test (get, put, patch, post, delete, head, options, ajax, json, xml)
+     * @return  bool Return true if test is ok or return false.
+     */
+    public function is($sTest)
+    {
+        $arsMethod = array('get', 'put', 'patch', 'post', 'delete', 'head', 'options');
+        if (in_array($sTest, $arsMethod)) {
+            if (strtolower($this->getMethod()) == $sTest) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($sTest == 'ajax') {
+            if (strtolower($this->getHeader('X-Requested-With')) == 'xmlhttprequest') {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($sTest == 'json') {
+            if ($this->isAcceptMimetype('application/json') === true) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($sTest == 'xml') {
+            if ($this->isAcceptMimetype('application/xml') === true || $this->isAcceptMimetype('text/xml') === true) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Check accept field
+     *
+     * @since   1.1
+     * @param   string $sContent Accept header.
+     * @param   string $test Accept test.
+     * @return  bool True if test is ok else false.
+     */
+    private function isAccept($acceptHeader, $test)
+    {
+        $acceptList = $this->parseAccept($acceptHeader);
+        if (empty($test)) {
+            return $acceptList;
+        } else {
+            foreach ($acceptList as $acceptType) {
+                if (in_array($test, $acceptType)) {
                     return true;
                 }
-                if (in_array('*/*', $arsAcceptList)) {
+                if (in_array('*/*', $acceptType)) {
                     return true;
                 }
             }
@@ -50,103 +421,101 @@ class Request
     /**
      * Check accept charset
      *
-     * @since   1.0
-     * @param   string $sCharset Test charset.
+     * @since   1.1
+     * @param   string $charset Test charset.
      * @return  bool True if test is ok else false.
      */
-    public function acceptCharset($sCharset)
+    public function isAcceptCharset($charset)
     {
-        return $this->accept($_SERVER['HTTP_ACCEPT_CHARSET'], $sCharset);
+        if ($this->hasHeader('Accept-Charset')) {
+            return $this->isAccept($this->getHeader('Accept-Charset'), $charset);
+        }
+        return false;
     }
 
     /**
      * Check accept encoding
      *
-     * @since   1.0
-     * @param   string $sEncoding Test encoding.
+     * @since   1.1
+     * @param   string $encoding Test encoding.
      * @return  bool True if test is ok else false.
      */
-    public function acceptEncoding($sEncoding)
+    public function isAcceptEncoding($encoding)
     {
-        return $this->accept($_SERVER['HTTP_ACCEPT_ENCODING'], $sEncoding);
+        if ($this->hasHeader('Accept-Encoding')) {
+            return $this->isAccept($this->getHeader('Accept-Encoding'), $encoding);
+        }
+        return false;
     }
 
     /**
      * Check accept language
      *
-     * @since   1.0
-     * @param   string $sLanguage Test language.
+     * @since   1.1
+     * @param   string $language Test language.
      * @return  bool True if test is ok else false.
      */
-    public function acceptLanguage($sLanguage)
+    public function isAcceptLanguage($language)
     {
-        return $this->accept($_SERVER['HTTP_ACCEPT_LANGUAGE'], $sLanguage);
+        if ($this->hasHeader('Accept-Language')) {
+            return $this->isAccept($this->getHeader('Accept-Language'), $language);
+        }
+        return false;
     }
 
     /**
      * Check accept mimetype
      *
-     * @since   1.0
-     * @param   string $sMimetype Test mimetype.
+     * @since   1.1
+     * @param   string $mimetype Test mimetype.
      * @return  bool True if test is ok else false.
      */
-    public function acceptMimetype($sMimetype)
+    public function isAcceptMimetype($mimetype)
     {
-        return $this->accept($_SERVER['HTTP_ACCEPT'], $sMimetype);
-    }
-
-    /**
-     * Get client IP
-     *
-     * @since   1.0
-     * @return  string Return client IP.
-     */
-    public function clientIp()
-    {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $sIp = preg_replace('/(?:,.*)/', '', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $sIp = $_SERVER['HTTP_CLIENT_IP'];
-        } else {
-            $sIp = $_SERVER['REMOTE_ADDR'];
+        if ($this->hasHeader('Accept')) {
+            return $this->isAccept($this->getHeader('Accept'), $mimetype);
         }
-        return trim($sIp);
+        return false;
     }
 
     /**
-     * Get domain
+     * Check ajax
      *
-     * @since   1.0
-     * @param   string $nTldLength Length of Top Level Domain. (by default 1)
-     * @return  string Return domain.
+     * @since   1.1
+     * @return  bool Return true if test is ok or return false.
      */
-    public function domain($nTldLength = 1)
+    public function isAjax()
     {
-        $arsSegments = explode('.', $this->host());
-        $arsDomain = array_slice($arsSegments, -1 * ($nTldLength + 1));
-        return implode('.', $arsDomain);
+        if (strtolower($this->getHeader('X-Requested-With')) == 'xmlhttprequest') {
+            return true;
+        }
+        return false;
     }
 
     /**
-     * Get host
+     * Check method
      *
-     * @since   1.0
-     * @return  string Return host.
+     * @since   1.1
+     * @return  bool Return true if test is ok or return false.
      */
-    public function host()
+    public function isMethod($methodName)
     {
-        return $_SERVER['HTTP_HOST'];
+        if ($this->getMethod() == strtoupper($methodName)) {
+            return true;
+        }
+        return false;
     }
 
     /**
      * Get request method
      *
      * @since   1.0
+     * @deprecated deprecated since version 1.1
      * @return  string Return request method (GET, POST, ...).
      */
     public function method()
     {
-        return $_SERVER['REQUEST_METHOD'];
+        return $this->getMethod();
     }
 
     /**
@@ -189,97 +558,60 @@ class Request
      * Get request port
      *
      * @since   1.0
+     * @deprecated deprecated since version 1.1
      * @return  integer Return request port.
      */
     public function port()
     {
-        return $_SERVER['SERVER_PORT'];
+        return $this->getPort();
     }
 
     /**
      * Get request scheme
      *
      * @since   1.0
+     * @deprecated deprecated since version 1.1
      * @return  string Return request scheme (http or https).
      */
     public function scheme()
     {
-        if (isset($_SERVER['HTTPS']) || is_empty($_SERVER['HTTPS'])) {
-            return 'https';
-        } else {
-            return 'http';
-        }
+        return $this->getScheme();
     }
 
     /**
      * Get subdomain
      *
      * @since   1.0
-     * @param   string $nTldLength Length of Top Level Domain. (by default 1)
+     * @deprecated deprecated since version 1.1
+     * @param   string $tldLength Length of Top Level Domain. (by default 1)
      * @return  string Return subdomain.
      */
-    public function subdomain($nTldLength = 1)
+    public function subdomain($tldLength = 1)
     {
-        $arsSegments = explode('.', $this->host());
-        return array_slice($arsSegments, 0, -1 * ($nTldLength + 1));
+        return $this->getSubdomain($tldLength);
     }
 
     /**
      * Get request url
      *
      * @since   1.0
+     * @deprecated deprecated since version 1.1
      * @return  string Return request url.
      */
     public function url()
     {
-        return $_SERVER['REQUEST_URI'];
+        return $this->getUrl();
     }
 
     /**
      * Get user agent
      *
      * @since   1.0
+     * @deprecated deprecated since version 1.1
      * @return  string Return user agent.
      */
     public function userAgent()
     {
-        return $_SERVER['HTTP_USER_AGENT'];
-    }
-
-    /**
-     * Check method, ajax, json and xml
-     *
-     * @since   1.0
-     * @param   string $sTest Type of test (get, put, patch, post, delete, head, options, ajax, json, xml)
-     * @return  bool Return true if test is ok or return false.
-     */
-    public function is($sTest)
-    {
-        $arsMethod = array('get', 'put', 'patch', 'post', 'delete', 'head', 'options');
-        if (in_array($sTest, $arsMethod)) {
-            if (strtolower($this->method()) == $sTest) {
-                return true;
-            } else {
-                return false;
-            }
-        } elseif ($sTest == 'ajax') {
-            if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-                return true;
-            } else {
-                return false;
-            }
-        } elseif ($sTest == 'json') {
-            if ($this->acceptMimetype('application/json') === true) {
-                return true;
-            } else {
-                return false;
-            }
-        } elseif ($sTest == 'xml') {
-            if ($this->acceptMimetype('application/xml') === true || $this->acceptMimetype('text/xml') === true) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        return $this->getUserAgent();
     }
 }
